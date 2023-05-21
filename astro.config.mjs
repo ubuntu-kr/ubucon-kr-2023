aa11c86ce9aec1d0cfa317e3ee41a3cec6ff8bc2
Youngbin Han <ybhan@ubuntu.com>
2023-05-22 01:45:55 +0900

    랜딩 페이지 작성

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    ssr: {
+      noExternal: ['vanilla-framework'],
+    }
+  }
+});
