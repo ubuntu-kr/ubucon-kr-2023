fa4986d77032f8d113f1d1c482d46b670325853c
Youngbin Han <ybhan@ubuntu.com>
2023-05-23 02:49:50 +0900

    Add i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,33 @@
+export const languages = {
+
+  ko: '한국어',
+  en: 'English',
+};
+
+export const defaultLang = 'ko';
+
+export const ui = {
+  ko: {
+    'eventMeta.date': '2023년 9월 9일',
+    'eventMeta.venue': '한국마이크로소프트*',
+    'eventMeta.venueDisclaimer': '*거의 확정 되었으나, 변동될 수 있음.',
+    'intro.uca23': '작년 UbuCon Asia에 이어, 올해는 UbuCon Korea가 열립니다.',
+    'intro.uca23desc': '작년에 이어 서울에서 오는 가을에 열립니다. 자세한 사항은 곧 이 웹사이트에 업데이트 됩니다. 포럼에 로그인 후 알림 설정을 하시면, 업데이트를 받으실 수 있습니다.',
+    'intro.visitForum':'우분투한국커뮤니티 포럼 방문',
+    'sponsorship.getNotified': '후원사 등록 안내 받기',
+    'sponsorship.notifyDesc': '행사 후원에 관심이 있으신사가요?, 이메일로 연락 주시면, 후원사 프로그램 정보가 나올 때 먼저 알려드리겠습니다.',
+    'sponsorship.notifyContact': '후원사 등록 알림 요청하기'
+  },
+  en: {
+    'eventMeta.date': '9th September 2023',
+    'eventMeta.venue': 'Microsoft Korea*',
+    'eventMeta.venueDisclaimer': '*Venue may subject to change',
+    'intro.uca23': "Following last year's UbuCon Asia, UbuCon Korea will be held.",
+    'intro.uca23desc': 'Coming this fall in Seoul. More information will be on this website soon. Login to our forum and enable notification to get notified for updates.',
+    'intro.visitForum':'Visit Ubuntu Korea Community Forum',
+    'sponsorship.getNotified': 'Get notified for Sponsorship informations',
+    'sponsorship.notifyDesc': 'Interested to sponsor us?, Contact us with Email, We will notify you once Sponsorship informations are ready.',
+    'sponsorship.notifyContact': 'Contact us to get notified'
+  },
+
+} as const;
