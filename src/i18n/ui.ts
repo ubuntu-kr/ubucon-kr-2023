88bb09991594d295e73171efd91f7d6dcfb9de89
Youngbin Han <sukso96100@gmail.com>
2023-05-26 01:26:48 +0900

    장소 미확정 표시 삭제

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,7 +10,7 @@ export const ui = {
   ko: {
     'eventMeta.date': '2023년 9월 9일',
     'eventMeta.theme': '개발자 생산성을 위한 우분투',
-    'eventMeta.venue': '한국마이크로소프트*',
+    'eventMeta.venue': '한국마이크로소프트',
     'eventMeta.venueDisclaimer': '*거의 확정 되었으나, 변동될 수 있음.',
     'intro.uca23': '작년 UbuCon Asia에 이어, 올해는 UbuCon Korea가 열립니다.',
     'intro.uca23desc': '작년에 이어 서울에서 오는 가을에 열립니다. 자세한 사항은 곧 이 웹사이트에 업데이트 됩니다. 포럼에 로그인 후 알림 설정을 하시면, 업데이트를 받으실 수 있습니다.',
@@ -22,7 +22,7 @@ export const ui = {
   en: {
     'eventMeta.date': '9th September 2023',
     'eventMeta.theme': 'Ubuntu for Developer Productivity',
-    'eventMeta.venue': 'Microsoft Korea*',
+    'eventMeta.venue': 'Microsoft Korea',
     'eventMeta.venueDisclaimer': '*Venue may subject to change',
     'intro.uca23': "Following last year's UbuCon Asia, UbuCon Korea will be held.",
     'intro.uca23desc': 'Coming this fall in Seoul. More information will be on this website soon. Login to our forum and enable notification to get notified for updates.',
