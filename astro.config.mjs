39336fcda4fbe17b416cf7bf8ae75747ca8e7116
Youngbin Han <ybhan@ubuntu.com>
2023-05-27 16:46:23 +0900

    후원사 페이지 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   vite: {
     ssr: {
-      noExternal: ['vanilla-framework'],
+      noExternal: ['vanilla-framework']
     }
   }
 });
