10c85500566a206baf5923d0eb224a11e771d3ea
paperbox <lego37yoon@outlook.com>
2023-06-06 17:29:56 +0900

    URL 오류 수정

--- src/components/DiscourseNoticeBanner.jsx
+++ src/components/DiscourseNoticeBanner.jsx
@@ -22,7 +22,7 @@ export default function DiscourseNoticeBanner(props) {
                         };
                     }).slice(0, 3);
                 setTopicList(filteredTopics);
-                setMoreTopic(data["topic_list"]["more_topics_url"]);
+                setMoreTopic(`https://discourse.ubuntu-kr.org${data["topic_list"]["more_topics_url"]}`);
             })
     }, [])
     return (
